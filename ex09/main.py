330d7f84df72c59588a191756a8e847c66749650
Rafael <rafarafa99944@gmail.com>
2024-08-09 23:50:51 -0300

    making ex09

---
+++ ex09/main.py
@@ -0,0 +1,38 @@
+##name = input("type ur name: ")
+##age = int( input("enter ur age: ") )
+##weight = float( input('enter ur weight: ') )
+##
+##print(name, age, weight)
+
+##def loop():
+##    for i in range(100):  
+##        i += 1
+##        print(i)
+##    return i
+##
+##loop()
+
+##def gradesAverage(g1, g2, g3):
+##    if (g1 + g2 + g3) / 3 >= 6:
+##        print("approved")
+##    else:
+##        print("desapproved")
+##        
+##gradesAverage(5, 5, 8)
+
+##def askName():
+##    name = "rafa"
+##    ask_name = input("type ur name: ")
+##    while ask_name != name:
+##        ask_name_again = input("type ur name again: ")
+##        ask_name = ask_name_again
+##        if ask_name != name:
+##            print(f"incorrect name, {ask_name}")
+##        else:
+##            print(f"you won, {ask_name}")
+##    return
+##
+##askName()
+
+
+        
