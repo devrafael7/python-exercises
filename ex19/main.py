47a3cfe912fb786b1a8c10af9e1cab7745a2cb28
Rafael <rafarafa99944@gmail.com>
2024-08-28 02:05:36 -0300

    ex19 finaly done

--- ex19/main.py
+++ ex19/main.py
@@ -1,13 +1,26 @@
 from flask import Flask, request, render_template
 import openpyxl
+import os
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
-book = openpyxl.Workbook()
-book.create_sheet('Form')
+excel_file = 'form_py.xlsx'
 
+if os.path.exists(excel_file):
+    book = openpyxl.load_workbook(excel_file)
+else:
+    book = openpyxl.Workbook()
+    book.create_sheet('Form')
+    
+    
 form_page = book['Form']
-form_page.append(['Purchaser Name', 'Gender', 'Locatioon', 'Product', 'Price', 'Quantity'])
+
+if 'Form' not in book.sheetnames or book['Form'].max_row == 1:
+    form_page.append(['Purchaser Name', 'Gender', 'Location', 'Product', 'Product ID', 'Price', 'Quantity', 'Date', 'Time', 'Order ID'])
+
+
 @app.route('/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
@@ -18,7 +31,26 @@ def form():
         price = request.form['price_value']
         quantity = request.form['quantity']
         
-        form_page.append([name, gender, location, product, price, quantity])
+        date = datetime.now().strftime('%Y-%m-%d')
+        time = datetime.now().strftime('%H:%M:%S')
+        
+        unique_PI = set()
+        
+        while True:
+            product_id = random.randint(1, 10000)
+            if product_id not in unique_PI:
+                unique_PI.add(product_id)
+            break
+        
+        unique_OD = set()
+        
+        while True:
+            order_id = f"ORD-{random.randint(10000, 99999)}"
+            if order_id not in unique_OD:
+                unique_OD.add(order_id)
+            break
+        
+        form_page.append([name, gender, location, product, product_id, price, quantity, date, time, order_id])
         book.save('form_py.xlsx')
 
         return 'deu bom mermo'
