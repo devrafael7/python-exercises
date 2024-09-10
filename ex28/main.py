ab258ad38a8453cfbce93d4e00d9b6be011585ec
Rafael <rafarafa99944@gmail.com>
2024-09-10 02:20:36 -0300

    making ex28

---
+++ ex28/main.py
@@ -0,0 +1,21 @@
+##client_choose = input('choose one of these operators: + | - | * | /: ')
+##client_choose_numbers0 = int(input('enter the first number: '))
+##client_choose_numbers1 = int(input('enter the second number: '))
+##
+##if client_choose == '+':
+##    calculator = client_choose_numbers0 + client_choose_numbers1
+##elif client_choose == '-':
+##    calculator = client_choose_numbers0 - client_choose_numbers1
+##elif client_choose == '*':
+##    calculator = client_choose_numbers0 * client_choose_numbers1
+##elif client_choose == '/':
+##    calculator = client_choose_numbers0 / client_choose_numbers1
+##else:
+##    print('choose one of these four operators')
+##print(calculator)
+
+##client_number = int(input('enter a number: '))
+##if client_number % 2 == 0:
+##    print('odd')
+##else:
+##    print('even')
