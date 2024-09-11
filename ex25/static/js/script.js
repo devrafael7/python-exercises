de5bc061ede24c799898284d3c9a58e56e083381
Rafael <rafarafa99944@gmail.com>
2024-09-11 00:25:47 -0300

    quantity card added

--- ex25/static/js/script.js
+++ ex25/static/js/script.js
@@ -2,9 +2,21 @@ const readmoreBtns = document.querySelectorAll('.readmoreBtn');
 
 readmoreBtns.forEach(readmoreBtn => {
     readmoreBtn.addEventListener('click', () => {
-        const card = readmoreBtn.closest('.card'); 
-        const cardImgSrc = card.querySelector('.cardImg').src; 
+        const readMoreCard = document.querySelector('.readMoreCard')
+        readMoreCard.classList.toggle('hidden')
 
      
     });
 });
+
+const quantityCard = document.querySelector('.quantityCard')
+quantity_total = 0
+
+const addToCartBtn = document.querySelectorAll('.addToCartBtn')
+addToCartBtn.forEach(addToCartBtns => {
+    addToCartBtns.addEventListener('click', ()=>{
+        quantity_total += 1
+        quantityCard.textContent = quantity_total
+    })
+})
+
