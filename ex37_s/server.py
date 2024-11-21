0095e69d79cda82639fa1993e3eb445b52dd5696
Rafael <rafarafa99944@gmail.com>
2024-11-20 22:55:08 -0300

    response sent from server to client (email already exists)

--- ex37_s/server.py
+++ ex37_s/server.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 register_db = 'register_db.xlsx'
 
+# Carregar o banco de dados de registros
 book = openpyxl.load_workbook(register_db)
 register_page = book.active
 
 @app.route('/', methods=['GET', 'POST'])
 def sign_up():
+    show_element = False  # Inicialmente, o elemento está oculto
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -22,15 +24,15 @@ def sign_up():
         email_exists = any(row[0] == email for row in register_page.iter_rows(values_only=True))
         
         if email_exists:
-            return render_template('sign_up.html', email_exists_on_db=True)
+            show_element = True  # Se o email já existir, o elemento será mostrado
+            return render_template('sign_up.html', email_exists_on_db=True, show_element=show_element)
         else:
             # Adicionar o novo registro
             register_page.append([email, password, register_date, register_time])
             book.save(register_db)
             return render_template('index.html')
         
-    return render_template('sign_up.html', email_exists_on_db=False)
+    return render_template('sign_up.html', email_exists_on_db=False, show_element=show_element)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
